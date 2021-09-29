1d5a3fd32984f0f25748cdeeb7c5964f4e9d1318
Ramirors31 <ramirezsiqueiros@hotmail.com>
2021-09-29 14:13:45 -0500

    Datepicker para seleccionar fecha de servicio

--- app/src/main/java/com/example/westcutsbarbershop/fragments/ServicesFragment.kt
+++ app/src/main/java/com/example/westcutsbarbershop/fragments/ServicesFragment.kt
@@ -1,15 +1,17 @@
 package com.example.westcutsbarbershop.fragments
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.example.westcutsbarbershop.R
-
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -24,25 +26,51 @@ class ServicesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val rootView =inflater.inflate(R.layout.fragment_services, container, false)
+        //  VISTA PADRE DEL FRAGMENTO
+        val rootView = inflater.inflate(R.layout.fragment_services, container, false)
 
-        val spinner:Spinner =rootView.findViewById<Spinner>(R.id.spinner_barberos)
+
+        //******INICIA CODIFICACION DE SPINNER PARA SELECCIONAR BARBERO*********
+        val spinner: Spinner = rootView.findViewById<Spinner>(R.id.spinner_barberos)
         val lista = resources.getStringArray(R.array.opciones)
-        val adaptador = ArrayAdapter<String>(spinner.context, android.R.layout.simple_spinner_item,lista)
+        val adaptador =
+            ArrayAdapter<String>(spinner.context, android.R.layout.simple_spinner_item, lista)
         spinner.adapter = adaptador
+        //******TERMINA CODIFICACION DE SPINNER***********
 
+        //DATE PICKER PARA FECHA DE CITA
+        val editFecha: EditText = rootView.findViewById<EditText>(R.id.edit_fecha)
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR,year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateLabel(myCalendar,editFecha)
+        }
 
+        editFecha.setOnClickListener{
+            DatePickerDialog(editFecha.context,datePicker,
+            myCalendar.get(Calendar.YEAR),
+            myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+        //NOTIFICACION DE SERVICIO AGENDADO
         val sendButton:Button = rootView.findViewById<Button>(R.id.btn_agendar)
         sendButton.setOnClickListener {
             val message = "Se ha agendado tu servicio"
             Toast.makeText(sendButton.context, message, Toast.LENGTH_SHORT).show()
         }
+        
         return rootView
-
 
     }
 
+     private fun updateLabel(myCalendar: Calendar,editFecha:EditText) {
+        val format = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(format,Locale.ENGLISH)
+        editFecha.setText(sdf.format(myCalendar.time))
 
+    }
 
 
 }
