3db8a1ccdf7a6faee74358090336f71a278a29c3
AndresJDLRmr <90889489+AndresJDLRmr@users.noreply.github.com>
2021-11-10 16:01:56 -0600

    Fragment para inicio de sesion de barberos

---
+++ app/src/main/java/com/example/westcutsbarbershop/fragments/BarbersCitasFragment.kt
@@ -0,0 +1,22 @@
+package com.example.westcutsbarbershop.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.westcutsbarbershop.R
+
+class BarbersCitasFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_barbers_citas, container, false)
+    }
+
+
+}
